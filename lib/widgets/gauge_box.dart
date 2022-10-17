f4fcf3877a3a319a824254bf5c77840e796429c7
kyu <2066apple@naver.com>
2022-10-17 22:51:24 +0900

    ✨ 키 측정용 슬라이드 위젯 구현

---
+++ lib/widgets/gauge_box.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class GaugeBox extends StatefulWidget {
+  final Text label;
+  final double limitNumber;
+  const GaugeBox({
+    super.key,
+    required this.label,
+    required this.limitNumber,
+  });
+
+  @override
+  State<GaugeBox> createState() => _GaugeBoxState();
+}
+
+class _GaugeBoxState extends State<GaugeBox> {
+  double _num = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    Size _size = MediaQuery.of(context).size;
+    return Container(
+      width: _size.width - 16,
+      height: _size.width / 3,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.blueAccent,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            width: _size.width,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(child: widget.label),
+                SizedBox(width: 20),
+                Text('${_num.toInt()}'),
+              ],
+            ),
+          ),
+          SizedBox(
+            width: _size.width,
+            child: Slider(
+              // min: 0,
+              max: widget.limitNumber,
+              thumbColor: Colors.cyanAccent,
+              activeColor: Colors.cyan,
+              divisions: widget.limitNumber.toInt(),
+              value: _num,
+              onChanged: (value) {
+                setState(() {
+                  _num = value;
+                });
+                setNumber(value);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void setNumber(double value) async {
+    SharedPreferences.setMockInitialValues({});
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setDouble('tall', value);
+  }
+}
